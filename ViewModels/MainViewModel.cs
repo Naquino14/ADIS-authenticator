8f4fd42810d299f5ba037b95cf7ba1aeb9155064
Nathaniel Aquino <naquino14@outlook.com>
2023-07-19 18:21:00 -0400

    Implememted code updating with threading

--- ViewModels/MainViewModel.cs
+++ ViewModels/MainViewModel.cs
@@ -2,21 +2,49 @@
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using System.Collections.ObjectModel;
+using System.Text;
 
 namespace ADIS_authenticator.ViewModels;
 public partial class MainViewModel : ObservableObject {
     [ObservableProperty]
-    ObservableCollection<string> codes;
+    ObservableCollection<OTPCode> codes;
 
     public MainViewModel() : base() {
         codes = new() {
-            "abcd",
-            "efg"
+            new("Test: naa4471@rit.edu", Encoding.ASCII.GetBytes("longerpassword!!"), 1245678, 10),
+            new("Test2: AAAAHHHS", Encoding.ASCII.GetBytes("longerpassword!!"), 12678, 10),
+            new("Test3: fast boi", Encoding.ASCII.GetBytes("differentpasswrd"), DateTimeOffset.UtcNow.ToUnixTimeSeconds(), 3)
         };
+        new Thread(UpdateCodesThread).Start();
     }
 
-    [RelayCommand]
-    void Update() {
-        Codes[0] = "AAAAAAAAAAAAA";
+    void UpdateCodesThread() {
+        for (; ; ) {
+            Thread.Sleep(500);
+            for (int i = 0; i < Codes.Count; i++) {
+                if (Codes[i].Code != Codes[i].lastCode) {
+                    var temp = Codes[i];
+                    Codes[i] = null;
+                    Codes[i] = temp;
+                    Codes[i].Code = Codes[i].Code;
+                }
+            }
+        }
+    }
+}
+
+public partial class OTPCode {
+    public string Code { get => AOTP.Code; set => lastCode = value; }
+    public string Label { get; set; }
+
+    private AOTP1 AOTP { get; init; }
+
+    public string lastCode;
+
+    public OTPCode(string label, byte[] key, long epoch, int duration) {
+        AOTP = new(key, epoch, duration);
+        Label = label;
+        Code = AOTP.Code;
+        lastCode = Code;
     }
 }
